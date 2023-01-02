3220cada6d02ac39e1f95ed0a12ed435435d84da
juanfh <juan.cambados.web@gmail.com>
2023-01-02 21:55:57 +0100

    mod next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+  },
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    runtimeCaching,
+  },
+  images: {
+    domains: ["localhost", "itunes.apple.com"],
+    formats: ["image/avif", "image/webp"]
+  },
+})
