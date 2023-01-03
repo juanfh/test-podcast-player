02c8419aceab1efe3ad8bcc005d932ef1bf1fbe6
juanfh <juan.cambados.web@gmail.com>
2023-01-03 19:09:31 +0100

    add link to detail card

--- components/podcast/PodcastDetailCard.tsx
+++ components/podcast/PodcastDetailCard.tsx
@@ -1,5 +1,6 @@
 import { Interweave } from "interweave"
 import Image from "next/image"
+import Link from "next/link"
 import { useEffect, useState } from "react"
 import { PodcastWithEpisodesProps } from "../../types/podcast"
 
@@ -16,12 +17,16 @@ export const PodcastDetailCard = ({ podcastDetail }: PodcastDetailCardProps) => 
 
   return (
     <div className="sm:sticky sm:top-4 place-self-start bg-white rounded-lg shadow-lg overflow-hidden p-4">
-      <div className="leading-0 w-full grid grid-cols-1 place-content-center aspect-1 relative rounded-full overflow-hidden">
-        <Image src={podcastDetail.image} alt={podcastDetail.title} fill className="object-cover" />
-      </div>
+      <Link href={`/podcast/${podcastDetail.id}`}>
+        <div className="leading-0 w-full grid grid-cols-1 place-content-center aspect-1 relative rounded-full overflow-hidden">
+          <Image src={podcastDetail.image} alt={podcastDetail.title} fill className="object-cover" />
+        </div>
+      </Link>
       <div className="pt-4 text-center">
-        <div className="text-xl font-bold text-fuchsia-800">{podcastDetail.title}</div>
-        <div className="text-xs text-fuchsia-600">by {podcastDetail.author}</div>
+        <Link href={`/podcast/${podcastDetail.id}`}>
+          <div className="text-xl font-bold text-fuchsia-800">{podcastDetail.title}</div>
+          <div className="text-xs text-fuchsia-600">by {podcastDetail.author}</div>
+        </Link>
         <div className="pt-4 text-left text-sm">
           <Interweave content={description} className="wysiwygeditor" />
         </div>
