16d2a9d8eb7f1d2e14139d5452ee0c30c9444f52
juanfh <juan.cambados.web@gmail.com>
2023-01-03 22:30:49 +0100

    fix detail card overflow

--- components/podcast/PodcastDetailCard.tsx
+++ components/podcast/PodcastDetailCard.tsx
@@ -16,7 +16,7 @@ export const PodcastDetailCard = ({ podcastDetail }: PodcastDetailCardProps) => 
   }, [podcastDetail])
 
   return (
-    <div className="group sm:sticky sm:top-4 place-self-start bg-white rounded-lg shadow-lg overflow-hidden p-4">
+    <div className="w-full group sm:sticky sm:top-4 place-self-start bg-white rounded-lg shadow-lg p-4">
       <Link href={`/podcast/${podcastDetail.id}`}>
         <div className="leading-0 w-full grid grid-cols-1 place-content-center aspect-1 relative rounded-full overflow-hidden">
           <Image src={podcastDetail.image} alt={podcastDetail.title} fill className="object-cover" />
@@ -27,11 +27,10 @@ export const PodcastDetailCard = ({ podcastDetail }: PodcastDetailCardProps) => 
           <div className="text-xl font-bold text-fuchsia-800 group-hover:text-fuchsia-900">{podcastDetail.title}</div>
           <div className="text-xs text-fuchsia-600 group-hover:text-fuchsia-700">by {podcastDetail.author}</div>
         </Link>
-        <div className="pt-4 text-left text-sm">
+        <div className="pt-4 text-left text-sm overflow-hidden">
           <Interweave content={description} className="wysiwygeditor" />
         </div>
       </div>
     </div>
   )
-
 }
