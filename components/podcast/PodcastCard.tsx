847740f1d2b3884124a47207108e8fdbc90a74ab
juanfh <juan.cambados.web@gmail.com>
2023-01-03 07:56:43 +0100

    short titles

--- components/podcast/PodcastCard.tsx
+++ components/podcast/PodcastCard.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image"
+import Link from "next/link"
+
 import { PodcastProps } from "../../types/podcast"
+import { getShortenedString } from "../../utils/getShortenedString"
 
 export interface PodcastCardProps {
   podcast: PodcastProps
@@ -8,15 +11,19 @@ export interface PodcastCardProps {
 export const PodcastCard = ({ podcast }: PodcastCardProps) => {
 
   return (
-    <div key={podcast.id} className="grid grid-cols-1 rounded-lg shadow-lg overflow-hidden p-4 gap-2">
-      <div className="leading-0 w-full grid grid-cols-1 place-content-center aspect-1 relative rounded-full overflow-hidden">
-        <Image src={podcast.image} alt={podcast.title} fill className="object-cover" />
-      </div>
-      <div className="text-center">
-        <div className="text-sm font-bold">{podcast.title}</div>
-        <div className="text-xs">{podcast.author}</div>
-      </div>
+
+    <div className="rounded-lg shadow-lg overflow-hidden p-4">
+      <Link href={`/podcast/${podcast.id}`}>
+        <div className="leading-0 w-full grid grid-cols-1 place-content-center aspect-1 relative rounded-full overflow-hidden">
+          <Image src={podcast.image} alt={podcast.title} fill className="object-cover" />
+        </div>
+        <div className="text-center pt-4">
+          <div className="text-sm font-bold">{getShortenedString(podcast.title)}</div>
+          <div className="text-xs">{getShortenedString(podcast.author)}</div>
+        </div>
+      </Link>
     </div>
+
   )
 
 }
