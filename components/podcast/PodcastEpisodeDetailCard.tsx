73df57220618cd4fd94da8f66bbb461ec72a29e6
juanfh <juan.cambados.web@gmail.com>
2023-01-03 18:40:23 +0100

    mod player styles

--- components/podcast/PodcastEpisodeDetailCard.tsx
+++ components/podcast/PodcastEpisodeDetailCard.tsx
@@ -6,12 +6,12 @@ export interface PodcastEpisodeDetailCardProps {
 }
 
 export const PodcastEpisodeDetailCard = ({ episodeDetail }: PodcastEpisodeDetailCardProps) => {
-  console.log('episodeDetail', episodeDetail)
+
   return (
     <div className="w-full sm:col-span-2">
       <div className=" bg-white rounded-lg shadow-lg overflow-hidden p-4 mb-4">
         <div className="text-xl font-bold text-fuchsia-800">{episodeDetail.title}</div>
-        <div className="pt-4 text-left text-sm">{episodeDetail.content}</div>
+        <div className="pt-4 text-left text-sm pb-8">{episodeDetail.content}</div>
         <Player url={episodeDetail.url} />
       </div>
     </div>
