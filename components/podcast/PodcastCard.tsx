0f3f7d1bdfa53bfa717bb28f2410b0ab9656b27a
juanfh <juan.cambados.web@gmail.com>
2023-01-03 07:27:39 +0100

    mod card image

--- components/podcast/PodcastCard.tsx
+++ components/podcast/PodcastCard.tsx
@@ -10,7 +10,7 @@ export const PodcastCard = ({ podcast }: PodcastCardProps) => {
   return (
     <div key={podcast.id} className="grid grid-cols-1 rounded-lg shadow-lg overflow-hidden p-4 gap-2">
       <div className="leading-0 w-full grid grid-cols-1 place-content-center aspect-1 relative rounded-full overflow-hidden">
-        <Image src={podcast.image} alt={podcast.title} width={300} height={300} />
+        <Image src={podcast.image} alt={podcast.title} fill className="object-cover" />
       </div>
       <div className="text-center">
         <div className="text-sm font-bold">{podcast.title}</div>
