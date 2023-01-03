c7b066118fda701a5a1142c48620b147ff2f4bed
juanfh <juan.cambados.web@gmail.com>
2023-01-03 07:31:50 +0100

    delete temp console logs

--- utils/localStorage/getFromLocalStorage.jsx
+++ utils/localStorage/getFromLocalStorage.jsx
@@ -4,16 +4,10 @@ export const getFromLocalStorage = (name) => {
   const now = new Date()
   const params = getAllFromLocalStorage()
   if (params && params[name]) {
-    console.log(`getFromLocalStorage: ${now.getTime()} - ${params.expiry}`)
     if (now.getTime() > params.expiry) {
-      console.log('getFromLocalStorage: expired')
-      // Remove the item from storage
       localStorage.removeItem('WebLocalParams')
       return false
-    } else {
-      return params[name]
     }
-
     return params[name]
   }
   return false
