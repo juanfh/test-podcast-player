e9af78d19619ae4996ee250d8b5a0655134697f4
juanfh <juan.cambados.web@gmail.com>
2023-01-03 16:38:26 +0100

    add color to detail card texts

--- components/podcast/PodcastDetailCard.tsx
+++ components/podcast/PodcastDetailCard.tsx
@@ -13,8 +13,8 @@ export const PodcastDetailCard = ({ podcastDetail }: PodcastDetailCardProps) => 
         <Image src={podcastDetail.image} alt={podcastDetail.title} fill className="object-cover" />
       </div>
       <div className="pt-4 text-center">
-        <div className="text-xl font-bold">{podcastDetail.title}</div>
-        <div className="text-xs">by {podcastDetail.author}</div>
+        <div className="text-xl font-bold text-fuchsia-800">{podcastDetail.title}</div>
+        <div className="text-xs text-fuchsia-600">by {podcastDetail.author}</div>
         <div className="pt-4 text-left text-sm">{podcastDetail.summary}</div>
       </div>
     </div>
