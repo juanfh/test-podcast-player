6006185a84dd65e43ac108d48b7809fbe3018ef4
juanfh <juan.cambados.web@gmail.com>
2023-01-06 11:28:34 +0100

    mod episodes list

--- components/podcast/PodcastEpisodesList.tsx
+++ components/podcast/PodcastEpisodesList.tsx
@@ -23,32 +23,46 @@ export const PodcastEpisodesList = ({ podcastDetail, locale, maintexts }: Podcas
           <div className="hidden md:grid md:grid-cols-6 items-center px-2 py-1 font-bold text-fuchsia-800">
             <div className="col-span-4 flex items-center gap-1">
               <div className="text-sm"><Icon icon="podcast" /></div>
-              <div>{maintexts.title}</div>
+              <div className="text-sm">{maintexts.title}</div>
             </div>
             <div className="flex justify-end items-center gap-1">
               <div className="text-xs"><Icon icon="calendar-days" /></div>
-              <div>{maintexts.date}</div>
+              <div className="text-sm">{maintexts.date}</div>
             </div>
             <div className="flex justify-end items-center gap-1">
               <div className="text-xs"><Icon icon="clock" /></div>
-              <div>{maintexts.duration}</div>
+              <div className="text-sm">{maintexts.duration}</div>
             </div>
           </div>
           {podcastDetail.episodes.map((episode, index) => (
-            <div key={episode.id} className={`grid grid-cols-1 md:grid-cols-6 items-center ${index % 2 === 0 ? "bg-white" : "bg-zinc-100"} px-2 py-1`}>
+            <div key={episode.id} className={`grid grid-cols-1 md:grid-cols-6 items-center ${index % 2 === 0 ? "bg-white" : "bg-zinc-100"} p-2`}>
               <Link href={`/podcast/${podcastDetail.id}/episode/${episode.id}`} className="md:col-span-4">
-                <div className="group text-sm text-fuchsia-600 flex items-center gap-2">
-                  <div className="flex flex-col justify-center items-center bg-fuchsia-800 group-hover:bg-fuchsia-900 transition-colors duration-300 text-white p-1 rounded-full w-8 h-8">
-                    <Icon icon="volume-high" />
+                <div className="group text-sm text-fuchsia-600 flex items-center gap-2 pb-1 md:pb-0">
+                  <div className="grid grid-cols-1 place-items-center">
+                    <div className="grid grid-cols-1 place-items-center bg-fuchsia-800 group-hover:bg-fuchsia-900 transition-colors duration-300 text-white text-xs rounded-full w-7 h-7">
+                      <Icon icon="volume-high" />
+                    </div>
                   </div>
                   <div className="w-full grow group-hover:text-fuchsia-800 transition-colors duration-300">
                     {episode.title}
                   </div>
                 </div>
               </Link>
-              <div className="grid grid-cols-1 md:grid-cols-2 md:col-span-2">
-                <div className="text-sm md:text-right">{episode.date ? formatDate(episode.date, locale) : "-"}</div>
-                <div className="text-sm md:text-right">{episode.duration ? formatSecondsToHours(episode.duration) : "-"}</div>
+              <div className="grid grid-cols-1 md:grid md:grid-cols-2 md:col-span-2 md:gap-1">
+                <div className="text-sm md:text-right flex md:block gap-1">
+                  <div className="flex md:hidden items-center gap-1">
+                    <div className="text-xs"><Icon icon="calendar-days" /></div>
+                    <div className="text-sm">{maintexts.date}:</div>
+                  </div>
+                  {episode.date ? formatDate(episode.date, locale) : "-"}
+                </div>
+                <div className="text-sm md:text-right flex md:block gap-1">
+                  <div className="flex md:hidden items-center gap-1">
+                    <div className="text-xs"><Icon icon="clock" /></div>
+                    <div className="text-sm">{maintexts.duration}:</div>
+                  </div>
+                  {episode.duration ? formatSecondsToHours(episode.duration) : "-"}
+                </div>
               </div>
             </div>
           ))}
