ded933a04e2cbf682cbd98c274794d97e4a23c53
juanfh <juan.cambados.web@gmail.com>
2023-01-06 10:14:15 +0100

    add button format

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -3,19 +3,38 @@ import { IconName } from "@fortawesome/fontawesome-svg-core"
 import { Icon } from "./Icon"
 
 export interface ButtonProps {
+  format?: "button" | "link"
   icon?: IconName
   label?: string
   onClick?: () => void
 }
 
-export const Button = ({ icon, label, onClick }: ButtonProps) => {
+export const ButtonContent = ({ icon, label }: ButtonProps) => {
 
   return (
-    <button className={`focus:outline-none px-5 py-2 text-sm font-normal text-white rounded bg-fuchsia-700 hover:bg-fuchsia-900 transition-colors duration-300`} onClick={onClick} >
-      <div className={`flex flex-nowrap min-w-max justify-center items-center ${(icon && label !== '') && "gap-1"}`}>
-        {icon && <Icon icon={icon} />}
-        {label && <div>{label}</div>}
-      </div>
-    </button>
+    <div className={`flex flex-nowrap min-w-max justify-center items-center ${(icon && label !== '') && "gap-1"}`}>
+      {icon && <Icon icon={icon} />}
+      {label && <div>{label}</div>}
+    </div>
+  )
+}
+
+export const Button = ({ format = "button", icon, label, onClick }: ButtonProps) => {
+
+  const className = "focus:outline-none px-5 py-2 text-sm font-normal text-white rounded bg-fuchsia-700 hover:bg-fuchsia-900 transition-colors duration-300"
+
+  return (
+    <>
+      {format === "button" ? (
+        <button className={className} onClick={onClick} >
+          <ButtonContent icon={icon} label={label} />
+        </button>
+      ) : (
+        <div className={className} onClick={onClick} >
+          <ButtonContent icon={icon} label={label} />
+        </div>
+      )}
+    </>
+
   )
 }
