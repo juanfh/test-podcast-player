ca6de314f522ddf0a3231a3ece659dff0f38376d
juanfh <juan.cambados.web@gmail.com>
2023-01-07 20:35:24 +0100

    fix data refresh

--- utils/getPodcastsList.tsx
+++ utils/getPodcastsList.tsx
@@ -9,23 +9,21 @@ export const getPodcastsList = async () => {
   const podcastLocalList = getFromLocalStorage('podcastList')
 
   if (podcastLocalList) {
-    saveToLocalStorage('podcastList', podcastLocalList)
     return podcastLocalList
-  } else {
-    try {
-      const newPodcastList = await getData({ url: "us/rss/toppodcasts/limit=100/genre=1310/json" })
-      if (newPodcastList?.feed?.entry) {
-        const mappedPodcastList = mapPodcasts(newPodcastList.feed.entry)
-        saveToLocalStorage('podcastList', mappedPodcastList)
-        return mappedPodcastList
-      } else {
-        return []
-      }
-    } catch (error) {
-      //console.log(error)
+  }
+
+  try {
+    const newPodcastList = await getData({ url: "us/rss/toppodcasts/limit=100/genre=1310/json" })
+    if (newPodcastList?.feed?.entry) {
+      const mappedPodcastList = mapPodcasts(newPodcastList.feed.entry)
+      saveToLocalStorage('podcastList', mappedPodcastList)
+      return mappedPodcastList
+    } else {
       return []
     }
-
+  } catch (error) {
+    //console.log(error)
+    return []
   }
 
 }
